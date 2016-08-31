c91fc6787e8815995ab3f24cf68653d149f0aeee
Your Name <yookuno@microsoft.com>
2016-08-31 15:44:38 +0100

    Added independence test

--- probability.py
+++ probability.py
@@ -6,11 +6,22 @@ def uniform(k):
     return [1.0 / k for _ in range(k)]
 
 
-def marginalize(P):
-    # marginalize P(i, j) over j to P(i)
-    return [sum(P[i][j] for j in range(len(P[0]))) for i in range(len(P))]
+def marginalize(P, axis=1):
+    if axis == 1:
+        # marginalize P(x, y) over y to P(x)
+        return [sum(P[i][j] for j in range(len(P[0]))) for i in range(len(P))]
+    elif axis == 0:
+        # marginalize P(x, y)) over x to P(y)
+        return [sum(P[i][j] for i in range(len(P))) for j in range(len(P[0]))]
+    raise Exception('Does not support axis', axis)
 
 
 def condition(P):
     # condition P(i, j) on j to P(i | j)
     return [[P[i][j] / sum(P[i][k] for k in range(len(P[0]))) for j in range(len(P[0]))] for i in range(len(P))]
+
+
+def is_independent(P):
+    P_x = marginalize(P, axis=1)
+    P_y = marginalize(P, axis=0)
+    return all(P[i][j] == P_x[i] * P_y[j] for i in range(len(P_x)) for j in range(len(P_y)))
--- test_probability.py
+++ test_probability.py
@@ -11,9 +11,15 @@ class TestProbability(TestCase):
 
     def test_uniform(self):
         self.assertEqual(uniform(2), [0.5, 0.5])
-        
+
     def test_marginalize(self):
-        self.assertEqual(marginalize([[0.1, 0.4], [0.3, 0.2]]), [0.5, 0.5])
+        self.assertEqual(marginalize([[0.1, 0.4], [0.3, 0.2]], axis=1), [0.5, 0.5])
+        self.assertEqual(marginalize([[0.1, 0.4], [0.3, 0.2]], axis=0), [0.4, 0.6000000000000001])
 
     def test_condition(self):
         self.assertEqual(condition([[0.1, 0.4], [0.3, 0.2]]), [[0.2, 0.8], [0.6, 0.4]])
+
+    def test_is_independent(self):
+        self.assertTrue(is_independent([[0.1, 0.4], [0.1, 0.4]]))
+        self.assertTrue(is_independent([[0.2, 0.2], [0.3, 0.3]]))
+        self.assertFalse(is_independent([[0.1, 0.4], [0.4, 0.1]]))
