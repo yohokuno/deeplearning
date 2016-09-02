940f45da5b9d4518baabec42c1fd753c4f2b8be0
Your Name <yookuno@microsoft.com>
2016-09-02 14:51:53 +0100

    Implementd Newton's method for scalar and decoupled iteration runner

--- optimization.py
+++ optimization.py
@@ -1,9 +1,34 @@
+import itertools
+import math
 
-def gradient_descent_scalar(cost_function, derivative_function, initial_value, iterations, learning_rate):
+
+def run_iterations(iterator, max_iterations, abs_tol=1e-20):
+    """ Run iterative optimization method such as gradient descent. Stop early if cost doesn't change. """
+    previous_cost = None
+    limited_iterator = itertools.islice(iterator, max_iterations)
+
+    for i, (x, cost) in enumerate(limited_iterator):
+        if previous_cost is not None and math.isclose(cost, previous_cost, abs_tol=abs_tol):
+            break
+        previous_cost = cost
+
+    return i, x, cost
+
+
+def gradient_descent_scalar(cost_function, derivative_function, initial_value, learning_rate):
     x = initial_value
 
-    for i in range(iterations):
+    while True:
         derivative = derivative_function(x)
         x -= learning_rate * derivative
-        cost = cost_function(x)
-        yield x, cost
+        yield x, cost_function(x)
+
+
+def newtons_method_scalar(cost_function, derivative_function, second_derivative_function, initial_value, learning_rate):
+    x = initial_value
+
+    while True:
+        derivative = derivative_function(x)
+        second_derivative = second_derivative_function(x)
+        x -= learning_rate * derivative / second_derivative
+        yield x, cost_function(x)
--- test_optimization.py
+++ test_optimization.py
@@ -2,12 +2,30 @@ from unittest import TestCase
 from optimization import *
 
 
+def cost_function(x):
+    return x * x
+
+
+def derivative_function(x):
+    return 2.0 * x
+
+
+def second_derivative_function(x):
+    return 2.0
+
+
 class TestOptimization(TestCase):
     def test_gradient_descent_scalar(self):
-        cost_function = lambda x: x * x
-        derivative_function = lambda x: 2 * x
-        iterator = gradient_descent_scalar(cost_function, derivative_function, 1.0, 100, 0.1)
-        for x, cost in iterator:
-            pass
+        # argmin_x x * x = 0
+        i, x, cost = run_iterations(gradient_descent_scalar(cost_function, derivative_function, 10.0, 0.1), 1000)
+        self.assertAlmostEqual(x, 0.0)
+        self.assertAlmostEqual(cost, 0.0)
+        self.assertGreater(i, 1)
+        self.assertLess(i, 1000-1)
+
+    def test_newtons_method_scalar(self):
+        # Quadratic function always converges with 1 iteration of Newton's method with learning ratio 1
+        i, x, cost = run_iterations(newtons_method_scalar(cost_function, derivative_function, second_derivative_function, 10.0, 1.0), 1000)
+        self.assertEqual(i, 1)
         self.assertAlmostEqual(x, 0.0)
         self.assertAlmostEqual(cost, 0.0)
