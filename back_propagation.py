2ea3b7248bc46da46cbb92100bcee7f155b947f6
Your Name <yookuno@microsoft.com>
2016-09-09 09:23:35 +0100

    WIP: implementing backprop

--- back_propagation.py
+++ back_propagation.py
@@ -1,11 +1,27 @@
-import numpy as np
+def identity(x):
+    return lambda x: x, lambda _: [1], x
+
+
+def multiply(x, y):
+    return lambda x, y: x * y, lambda x, y: [y, x], [x, y]
 
 
 def forward(inputs, functions):
     units = inputs
 
-    for function, parents in functions:
+    for function, _, parents in functions:
         arguments = [units[parent] for parent in parents]
         units.append(function(*arguments))
 
-    return units[-1]
+    return units
+
+
+def back_propagation(inputs, functions):
+    units = forward(inputs, functions)
+    gradients = [1]
+    for i in range(len(units), 0, -1):
+        unit = units[i-1]
+        _, gradient, parents = functions[i-len(inputs)]
+        arguments = [units[parent] for parent in parents]
+        gradients.insert(0, sum(gradients[parent] * gradient(arguments)[parent] for parent in parents))
+    return gradients
--- test_back_propagation.py
+++ test_back_propagation.py
@@ -6,15 +6,15 @@ class TestBackPropagation(unittest.TestCase):
     def test_forward(self):
         # Identify function
         inputs = [1]
-        functions = [[lambda x: x, [0]]]
+        functions = [identity([0])]
         output = forward(inputs, functions)
-        self.assertEqual(output, 1)
-
+        self.assertEqual(output, [1, 1])
+        """
         # 1 + 2 * 3
         inputs = [1, 2, 3]
         functions = [[lambda x, y: x * y, [1, 2]], [lambda x, y: x + y, [0, 3]]]
         output = forward(inputs, functions)
-        self.assertEqual(output, 7)
+        self.assertEqual(output[-1], 7)
 
         # W @ x + b
         W = np.random.rand(10, 5)
@@ -23,4 +23,16 @@ class TestBackPropagation(unittest.TestCase):
         inputs = [W, x, b]
         functions = [[lambda x, y: x @ y, [0, 1]], [lambda x, y: x + y, [3, 2]]]
         output = forward(inputs, functions)
-        np.testing.assert_almost_equal(output, inputs[0] @ inputs[1] + inputs[2])
+        np.testing.assert_almost_equal(output[-1], inputs[0] @ inputs[1] + inputs[2])
+        """
+
+    def test_backward(self):
+        inputs = [1]
+        functions = [identity([0])]
+        gradients = back_propagation(inputs, functions)
+        self.assertEqual(gradients, [1, 1, 1])
+
+        inputs = [2, 3]
+        functions = [multiply([0, 1])]
+        gradients = back_propagation(inputs, functions)
+        self.assertEqual(gradients, [3, 2, 1])
