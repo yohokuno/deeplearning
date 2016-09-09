9d27f0aaa8636e21f7beb42b90ba6dfa18283d02
Yoh Okuno <yookuno@microsoft.com>
2016-09-09 12:22:57 +0100

    Implemented back propagation with scalars and single child

--- back_propagation.py
+++ back_propagation.py
@@ -1,22 +1,9 @@
-import numpy as np
-
-
-def forward(inputs, functions):
-    units = inputs
-
-    for function, parents in functions:
-        arguments = [units[parent] for parent in parents]
-        units.append(function(*arguments))
-
-    return units
-
-
 def identity(i):
     def function(x):
         return x
 
     def gradient(_):
-        return 1
+        return [1]
 
     return function, gradient, [i]
 
@@ -31,9 +18,20 @@ def multiply(i, j):
     return function, gradient, [i, j]
 
 
+def add(i, j):
+    def function(x, y):
+        return x + y
+
+    def gradient(x, y):
+        return 1, 1
+
+    return function, gradient, [i, j]
+
+
 def back_propagation(inputs, functions):
-    # An unit is tuple of unit value, gradient, and indices of children.
-    units = [(input, None, []) for input in inputs]
+    # An unit is tuple of unit value, local gradient (gradient value wrt child) and indices of children.
+    # TODO: support multiple children
+    units = [[input, 0, []] for input in inputs]
 
     for function, gradient, parents in functions:
         arguments = []
@@ -42,17 +40,19 @@ def back_propagation(inputs, functions):
             arguments.append(units[parent][0])
             units[parent][2].append(len(units))
 
-        value = function(*arguments)
-        units.append((value, gradient, []))
+        units.append([function(*arguments), 0, []])
+
+        for parent, gradient_value in zip(parents, gradient(*arguments)):
+            units[parent][1] = gradient_value
 
     gradients = [0 for _ in range(len(units))]
     gradients[len(units)-1] = 1
 
     for j in range(len(units) - 1, 0, -1):
-        value, _, children = units[j-1]
+        value, gradient, children = units[j-1]
 
         for index in range(len(children)):
             child = children[index]
-            gradients[j-1] += gradients[child] # * gradient_ij
+            gradients[j-1] += gradients[child] * gradient
 
     return gradients
--- test_back_propagation.py
+++ test_back_propagation.py
@@ -3,30 +3,8 @@ from back_propagation import *
 
 
 class TestBackPropagation(unittest.TestCase):
-    def test_forward(self):
-        # Identify function
-        inputs = [1]
-        functions = [(lambda x: x, [0])]
-        output = forward(inputs, functions)
-        self.assertEqual(output, [1, 1])
-
-        # 1 + 2 * 3
-        inputs = [1, 2, 3]
-        functions = [(lambda x, y: x * y, [1, 2]), (lambda x, y: x + y, [0, 3])]
-        output = forward(inputs, functions)
-        self.assertEqual(output[-1], 7)
-
-        # W @ x + b
-        W = np.random.rand(10, 5)
-        x = np.random.rand(5, 1)
-        b = np.random.rand(10, 1)
-        inputs = [W, x, b]
-        functions = [(lambda x, y: x @ y, [0, 1]), (lambda x, y: x + y, [3, 2])]
-        output = forward(inputs, functions)
-        np.testing.assert_almost_equal(output[-1], inputs[0] @ inputs[1] + inputs[2])
-
-    def test_backward(self):
-        inputs = [1]
+    def test_back_propagation(self):
+        inputs = [2]
         functions = [identity(0)]
         gradients = back_propagation(inputs, functions)
         self.assertEqual(gradients, [1, 1])
@@ -35,3 +13,8 @@ class TestBackPropagation(unittest.TestCase):
         functions = [multiply(0, 1)]
         gradients = back_propagation(inputs, functions)
         self.assertEqual(gradients, [3, 2, 1])
+
+        inputs = [2, 3, 4]
+        functions = [multiply(0, 1), add(2, 3)]
+        gradients = back_propagation(inputs, functions)
+        self.assertEqual(gradients, [3, 2, 1, 1, 1])
