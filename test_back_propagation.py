c6d6a4ad9bea0cfabe625421f9a3d4641b5a5fa5
Your Name <yookuno@microsoft.com>
2016-09-18 15:46:19 +0100

    WIP: trying to implement multi layer perceptron

--- test_back_propagation.py
+++ test_back_propagation.py
@@ -174,3 +174,38 @@ class TestBackPropagation(TestCase):
 
         self.assertAlmostEqual(w.evaluate(), 1)
         self.assertAlmostEqual(J.evaluate(), 0)
+
+    def test_xor(self):
+        X = Variable(np.array([[0, 0], [1, 0], [0, 1], [1, 1]]))
+        y = Variable(np.array([0, 1, 1, 0]))
+        W = Variable(np.array([[1, 1], [1, 1]]))
+        c = Variable(np.array([0, -1]))
+        w = Variable(np.array([1, -2]))
+
+        np.testing.assert_almost_equal((X @ W).evaluate(), np.array([[0, 0], [1, 1], [1, 1], [2, 2]]))
+        np.testing.assert_almost_equal((X @ W + c).evaluate(), np.array([[0, -1], [1, 0], [1, 0], [2, 1]]))
+        np.testing.assert_almost_equal(Relu(X @ W + c).evaluate(), np.array([[0, 0], [1, 0], [1, 0], [2, 1]]))
+        np.testing.assert_almost_equal((Relu(X @ W + c) @ w).evaluate(), np.array([0, 1, 1, 0]))
+
+    def test_multi_layer_perceptron(self):
+        X = Variable(np.array([[0, 0], [1, 0], [0, 1], [1, 1]]))
+        y = Variable(np.array([0, 1, 1, 0]))
+        W1 = Variable(np.random.rand(2, 2))
+        c = Variable(np.random.rand(2))
+        w2 = Variable(np.random.rand(2))
+        p = Relu(X @ W1 + c) @ w2
+        J = Sum((y - p) ** 2)
+        dW1 = differentiate(J, W1)
+        dc = differentiate(J, c)
+        dw2 = differentiate(J, w2)
+
+        learning_rate = 0.5
+        for i in range(10):
+            w2.set_value(w2.evaluate() - learning_rate * dw2.evaluate())
+            c.set_value(c.evaluate() - learning_rate * dc.evaluate())
+            W1.set_value(W1.evaluate() - learning_rate * dW1.evaluate())
+
+        self.assertAlmostEqual(J.evaluate(), 0)
+        np.testing.assert_almost_equal(W1.evaluate(), np.array([[1, 1], [1, 1]]))
+        np.testing.assert_almost_equal(c.evaluate(), np.array([0, -1]))
+        np.testing.assert_almost_equal(w2.evaluate(), np.array([1, -2]))
