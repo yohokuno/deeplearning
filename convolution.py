1d6703d2796e30364c57e841de09f539ebb31fcf
Yoh Okuno <yookuno@microsoft.com>
2016-09-28 16:09:50 +0100

    Added basic convolution operations

---
+++ convolution.py
@@ -0,0 +1,24 @@
+def convolution1d_function(x, w):
+    def function(t, domain):
+        return sum(x(a) * w(t - a) for a in domain)
+    return function
+
+
+def convolution2d_function(I, K):
+    def function(i, j, domain_m, domain_n):
+        return sum(I(i-m, j-n) * K(m, n) for m in domain_m for n in domain_n)
+    return function
+
+
+def cross_correlation_function(I, K):
+    def function(i, j, domain_m, domain_n):
+        return sum(I(i + m, j + n) * K(m, n) for m in domain_m for n in domain_n)
+    return function
+
+
+def convolution1d(I, K):
+    return [sum(I[i+m] * K[m] for m in range(len(K))) for i in range(len(I)-len(K)+1)]
+
+
+def convolution2d(I, K):
+    return [[sum(I[i+m][j+n] * K[m][n] for m in range(len(K)) for n in range(len(K[0]))) for j in range(len(I[0])-len(K[0])+1)] for i in range(len(I)-len(K)+1)]
---
+++ test_convolution.py
@@ -0,0 +1,49 @@
+from unittest import TestCase
+from convolution import *
+
+
+class TestConvolution(TestCase):
+    def test_convolution1d_function(self):
+        function = convolution1d_function(lambda t: t, lambda t: t)
+        actual = function(0, range(5))
+        self.assertEqual(actual, -30)
+
+        actual = function(1, range(5))
+        self.assertEqual(actual, -20)
+
+        actual = function(0, range(-2, 3))
+        self.assertEqual(actual, -10)
+
+    def test_convolution2d_function(self):
+        image = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+        kernel = [[0, 1], [1, 0]]
+        function = convolution2d_function(lambda i, j: image[i][j], lambda m, n: kernel[m][n])
+        actual = function(1, 1, range(2), range(2))
+        self.assertEqual(actual, 4)
+
+        actual = function(2, 2, range(2), range(2))
+        self.assertEqual(actual, 12)
+
+    def test_cross_correlation_function(self):
+        image = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+        kernel = [[0, 1], [1, 0]]
+        function = cross_correlation_function(lambda i, j: image[i][j], lambda m, n: kernel[m][n])
+        actual = function(0, 0, range(2), range(2))
+        self.assertEqual(actual, 4)
+
+        actual = function(1, 1, range(2), range(2))
+        self.assertEqual(actual, 12)
+
+    def test_convolution1d(self):
+        I = [1, 2, 3]
+        K = [1, -1]
+        actual = convolution1d(I, K)
+        expected = [-1, -1]
+        self.assertSequenceEqual(actual, expected)
+
+    def test_convolution2d(self):
+        I = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+        K = [[0, 1], [1, 0]]
+        actual = convolution2d(I, K)
+        expected = [[4, 6], [10, 12]]
+        self.assertSequenceEqual(actual, expected)
