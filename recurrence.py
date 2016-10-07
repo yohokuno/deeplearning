f071ce22af75702ddec33f99f865c7b4926900d3
Yoh Okuno <yookuno@microsoft.com>
2016-10-07 15:42:13 +0100

    Implemented RNN gradient and SGD with it

--- recurrence.py
+++ recurrence.py
@@ -22,12 +22,38 @@ def rnn_predict(X, U, V, W, h_init):
         yield y
 
 
-def rnn_loss(X, U, V, W, h_init):
+def rnn_loss(X, Y_, U, V, W, h_init):
     h = h_init
 
-    for i in range(len(X)-1):
-        x = X[i]
-        y_ = X[i+1]
-        h = np.tanh(W @ h + U @ x)
+    for i in range(len(X)):
+        h = np.tanh(W @ h + U @ X[i])
         y = softmax(V @ h)
-        yield np.sum(-y_ * np.log(y))
+        yield np.sum(-Y_[i] * np.log(y))
+
+
+def rnn_gradient(X, Y_, U, V, W, h_init):
+    # Forward
+    h = h_init
+    H = []
+    Y = []
+
+    for i in range(len(X)):
+        h = np.tanh(W @ h + U @ X[i])
+        y = softmax(V @ h)
+        H.append(h)
+        Y.append(y)
+
+    # Backward
+    dV = np.zeros_like(V)
+    dW = np.zeros_like(W)
+    dU = np.zeros_like(U)
+    dh = V.T @ (Y[-1] - Y_[-1])
+
+    for i in range(len(X) - 2, -1, -1):
+        do = Y[i] - Y_[i]
+        dh = W.T @ dh @ np.diag(1 - H[i+1] ** 2) + V.T @ do
+        dV += np.outer(do, H[i])
+        dW += np.outer(np.diag(1 - H[i] ** 2) @ dh, H[i-1])
+        dU += np.outer(np.diag(1 - H[i] ** 2) @ dh, X[i])
+
+    return dV, dW, dU
--- test_recurrence.py
+++ test_recurrence.py
@@ -36,10 +36,28 @@ class TestRecurrence(TestCase):
 
     def test_rnn_loss(self):
         X = one_hot(np.array([0, 0, 1, 1]))
+        Y_ = one_hot(np.array([0, 1, 1, 1]))
         U = np.array([[0, 1], [1, 0]])
         V = np.array([[1, 0], [0, 1]])
         W = np.array([[0, 0], [0, 0]])
         h_init = np.array([0, 0])
-        Y = list(rnn_loss(X, U, V, W, h_init))
-        np.testing.assert_almost_equal(Y[0], Y[2])
-        np.testing.assert_array_less(Y[1], Y[0])
+        L = list(rnn_loss(X, Y_, U, V, W, h_init))
+        np.testing.assert_almost_equal(L[0], L[2])
+        np.testing.assert_array_less(L[1], L[0])
+
+    def test_rnn_gradient(self):
+        X = one_hot(np.array([0, 0, 1, 1]))
+        Y_ = one_hot(np.array([0, 1, 1, 0]))
+        U = np.array([[1., 0.], [0., 1.]])
+        V = np.array([[1., 0.], [0., 1.]])
+        W = np.array([[1., 0.], [0., 1.]])
+        h_init = np.array([0, 0])
+
+        for i in range(1000):
+            dV, dW, dU = rnn_gradient(X, Y_, U, V, W, h_init)
+            V -= 0.2 * dV
+            W -= 0.2 * dW
+            U -= 0.2 * dU
+
+        Y = np.array(list(rnn_predict(X, U, V, W, h_init)))
+        np.testing.assert_almost_equal(np.argmax(Y, axis=1), np.argmax(Y_, axis=1))
