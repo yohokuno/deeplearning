f813a5a7c2432015ddac60a86b2c140e7b296bd6
Yoh Okuno <yookuno@microsoft.com>
2016-10-24 08:32:12 +0100

    Added RBM

---
+++ rbm.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+
+class RBM:
+    def __init__(self, v_size, h_size, W, b, c):
+        self.v_size = v_size
+        self.h_size = h_size
+        self.W = W
+        self.b = b
+        self.c = c
+
+    def energy(self, v, h):
+        return -v.T @ self.W @ h - self.b @ v - self.c @ h
+
+    def partition(self):
+        v = self.__class__.binary(self.v_size).T
+        h = self.__class__.binary(self.h_size).T
+        return np.sum(np.exp(-self.energy(v, h)))
+
+    def joint(self, v, h):
+        return np.exp(-self.energy(v, h)) / self.partition()
+
+    @staticmethod
+    def binary(size):
+        x = np.arange(2 ** size, dtype=np.uint8)
+        y = np.expand_dims(x, 1)
+        z = np.unpackbits(y, 1)
+        return z[:, 8 - size:]
---
+++ test_rbm.py
@@ -0,0 +1,40 @@
+from unittest import TestCase
+from rbm import *
+
+
+class TestRBM(TestCase):
+    def test_binary(self):
+        actual = RBM.binary(2)
+        expected = np.array([[0, 0],
+                             [0, 1],
+                             [1, 0],
+                             [1, 1]])
+        np.testing.assert_almost_equal(actual, expected)
+
+    def test_rbm(self):
+        W = np.array([[1, 0], [0, 1]])
+        b = np.array([0, 0])
+        c = np.array([0, 0])
+        rbm = RBM(2, 2, W, b, c)
+
+        v = np.array([1, 0])
+        h = np.array([1, 0])
+        actual = rbm.energy(v, h)
+        expected = -1
+        self.assertAlmostEqual(actual, expected)
+
+        v = np.array([0, 1])
+        h = np.array([1, 0])
+        actual = rbm.energy(v, h)
+        expected = 0
+        self.assertAlmostEqual(actual, expected)
+
+        actual = rbm.partition()
+        expected = 9.0
+        self.assertAlmostEqual(actual, expected)
+
+        v = np.array([1, 0])
+        h = np.array([1, 0])
+        actual = rbm.joint(v, h)
+        expected = 0.30203131427322721
+        self.assertAlmostEqual(actual, expected)
